91a836f8cc7f2ccb2321f1c0b4c193321668c232
Kamlesh Saini <kamleshsaini1345@gmail.com>
2024-12-27 18:34:17 +0530

    db

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 function connectDb(){
-    mongoose.connect(process.env.DB_CONNECT)
+    mongoose.connect('mongodb+srv://UBER:EykZXYcueAPMGk2b@cluster1.b2ik1.mongodb.net/UBER')
     .then(() => {
         console.log('Connected to DB');
     }).catch(err => console.error("MongoDB connection error:", err));
